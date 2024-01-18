c2d9d1a2c7af91f810e6655421eb4d410ff28eda
HuijeWoo <152831616+HuijeWoo@users.noreply.github.com>
2024-01-18 13:58:45 +0900

    .py 안 붙였네..?

---
+++ unknown/blueprints/win01.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+win01_page = Blueprint('win01_page', __name__, 
+                       template_folder='templates',
+                       url_prefix='/win01')
+
--- unknown/start.py
+++ unknown/start.py
@@ -1,6 +1,6 @@
 import pymysql
 from flask import *
-from unknown.blueprints.win01_page import win01_page
+from unknown.blueprints.win01 import win01_page
 
 app = Flask(__name__)
 
@@ -8,4 +8,4 @@ app.register_blueprint(win01_page)
 
 @app.route('/')
 def home():
-    return render_template('')
+    return render_template('home.html')
